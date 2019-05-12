f0fb76e4cd2d7c6f5a28e437a24fea6ec562619b
Erin Millard <ezzatron@gmail.com>
2019-05-12 20:48:15 +1000

    Convert data to JS definitions

---
+++ src/definition/style.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'apple-touch-icon-scale': {
+    transform: 'scale(1.8)',
+  },
+  'safari-mask-icon-scale': {
+    transform: 'scale(.8)',
+  },
+  'social-share-icon-scale': {
+    transform: 'scale(.5)',
+  },
+  'windows-tile-icon-position': {
+    transform: 'translate(0, -10vh)',
+  },
+}
