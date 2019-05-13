f1da89f04ad8d4f1cfe2bffb35354f1ee2b7ba67
Erin Millard <ezzatron@gmail.com>
2019-05-13 22:11:52 +1000

    Add withTempDir to the filesystem

--- src/fs.js
+++ src/fs.js
@@ -1,17 +1,38 @@
 const rmfr = require('rmfr')
+const {join} = require('path')
 const {promisify} = require('util')
+const {tmpdir} = require('os')
 
 module.exports = {
   promisifyFileSystem,
 }
 
 function promisifyFileSystem (fs) {
-  const {mkdir, readFile, writeFile} = fs
+  const {mkdir, mkdtemp, readFile, writeFile} = fs
 
-  return {
+  const promisified = {
     mkdir: promisify(mkdir),
+    mkdtemp: promisify(mkdtemp),
     readFile: promisify(readFile),
     rmfr: async filePath => rmfr(filePath, fs),
     writeFile: promisify(writeFile),
   }
+
+  promisified['withTempDir'] = withTempDir.bind(null, promisified)
+
+  return promisified
+}
+
+async function withTempDir (fs, fn) {
+  const {mkdtemp} = fs
+  const tempDirPath = await mkdtemp(join(tmpdir(), 'iconduit-'))
+  let result
+
+  try {
+    result = await fn(tempDirPath)
+  } finally {
+    await fs.rmfr(tempDirPath)
+  }
+
+  return result
 }
