2afa851c229b06025dfd49a304cd48ae76c4611a
Erin Millard <ezzatron@gmail.com>
2019-05-13 21:56:35 +1000

    Add rmfr to the file system

--- src/fs.js
+++ src/fs.js
@@ -1,3 +1,4 @@
+const rmfr = require('rmfr')
 const {promisify} = require('util')
 
 module.exports = {
@@ -10,6 +11,7 @@ function promisifyFileSystem (fs) {
   return {
     mkdir: promisify(mkdir),
     readFile: promisify(readFile),
+    rmfr: async filePath => rmfr(filePath, fs),
     writeFile: promisify(writeFile),
   }
 }
