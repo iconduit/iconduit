5ee31d61a39a691ceb30118b510f8ded3f51bde0
Erin Millard <ezzatron@gmail.com>
2019-05-17 14:08:30 +1000

    Fix iPhone 6/6s/7/8 Plus DPI

--- src/definition/display.js
+++ src/definition/display.js
@@ -5,7 +5,7 @@ module.exports = {
   'ipad-1668x2388d264': {resolution: {horizontal: 1668, vertical: 2388}, pixelDensity: 264, pixelRatio: 2, orientation: 'portrait'},
   'ipad-2048x2732d264': {resolution: {horizontal: 2048, vertical: 2732}, pixelDensity: 264, pixelRatio: 2, orientation: 'portrait'},
   'iphone-1125x2436': {resolution: {horizontal: 1125, vertical: 2436}, pixelDensity: 458, pixelRatio: 3, orientation: 'portrait'},
-  'iphone-1242x2208': {resolution: {horizontal: 1242, vertical: 2208}, pixelDensity: 401, pixelRatio: 3, orientation: 'portrait'},
+  'iphone-1242x2208': {resolution: {horizontal: 1242, vertical: 2208}, pixelDensity: 462, pixelRatio: 3, orientation: 'portrait'},
   'iphone-1242x2688': {resolution: {horizontal: 1242, vertical: 2688}, pixelDensity: 458, pixelRatio: 3, orientation: 'portrait'},
   'iphone-640x1136': {resolution: {horizontal: 640, vertical: 1136}, pixelDensity: 326, pixelRatio: 2, orientation: 'portrait'},
   'iphone-750x1334': {resolution: {horizontal: 750, vertical: 1334}, pixelDensity: 326, pixelRatio: 2, orientation: 'portrait'},
