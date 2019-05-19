18866f10e61a69751aa9952d014a651ff5ea172c
Erin Millard <ezzatron@gmail.com>
2019-05-19 21:23:15 +1000

    Improved Apple touch icon scale definition

--- src/definition/style.js
+++ src/definition/style.js
@@ -1,17 +1,17 @@
 module.exports = {
   'apple-touch-icon-scale': {
-    transform: 'scale(1.8)',
-  },
-  'social-share-icon-scale': {
-    transform: 'scale(.5)',
+    // the difference between the Android large circle keyline diameter (52/108)
+    // and approximate iOS icon grid large circle size (892/1024)
+    // see https://itunes.apple.com/us/book/icon-design/id873778893
+    transform: 'scale(calc(6021 / 3328))',
   },
   'windows-tile-icon-position': {
-    // this is the difference between the standard Android square keyline size (44/108) and an exact 50% size
+    // the difference between the Android square keyline size (44/108) and an exact 50% size
     // see https://docs.microsoft.com/en-us/windows/uwp/design/style/app-icons-and-logos#tile-padding-recommendations
     transform: 'scale(calc(27 / 22))',
   },
   'windows-tile-icon-position-small': {
-    // this is the difference between the standard Android square keyline size (44/108) and an exact 66% size
+    // the difference between the Android square keyline size (44/108) and an exact 66% size
     // see https://docs.microsoft.com/en-us/windows/uwp/design/style/app-icons-and-logos#tile-padding-recommendations
     transform: 'scale(calc(81 / 50))',
   },
