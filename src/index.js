474ec7fc0881854d5200692787cc31ecc28130d3
Erin Millard <ezzatron@gmail.com>
2019-05-24 09:58:48 +1000

    Switch example config to .js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const {normalize} = require('./config.js')
 const services = require('./services.js')
 
 async function main (services) {
-  const {build, fileSystem: {readFile, withTempDir}, screenshotManager: {run}} = services
+  const {build, fileSystem: {withTempDir}, screenshotManager: {run}} = services
 
   const fixturePath = join(__dirname, '../test/fixture')
   const userInputDir = join(fixturePath, 'input')
-  const configPath = join(userInputDir, 'iconduit.config.json')
+  const configPath = join(userInputDir, 'iconduit.config.js')
   const outputPath = join(fixturePath, 'output')
 
-  const config = normalize(JSON.parse(await readFile(configPath)))
+  const config = normalize(require(configPath))
 
   await withTempDir(async tempPath => {
     const options = {configPath, outputPath, tempPath, userInputDir}
