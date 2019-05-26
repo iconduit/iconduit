d31e9ccae8d797ce8e83a02d82482908c749ae33
Erin Millard <ezzatron@gmail.com>
2019-05-26 17:32:26 +1000

    Tweaks to Iconduit fixture design

--- test/fixture/iconduit/dist/service-worker.js
+++ test/fixture/iconduit/dist/service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("cache-1558847742926")
+    caches.open("cache-1558855847095")
       .then(cache => cache.add('.'))
   )
 })
