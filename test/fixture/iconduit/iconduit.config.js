2cda004f87620ee81edbf0fd168cabc7afe8888e
Erin Millard <ezzatron@gmail.com>
2019-06-03 22:23:28 +1000

    Stop updating service worker

--- test/fixture/iconduit/iconduit.config.js
+++ test/fixture/iconduit/iconduit.config.js
@@ -29,6 +29,9 @@ module.exports = () => {
       include: [
         'appleTouchIconMasked',
       ],
+      exclude: [
+        'serviceWorker',
+      ],
     },
     extra: {
       license: 'MIT',
