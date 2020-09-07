2e3424aca59689a3dd1d0e2203ee66b5fb7e12cb
Erin Millard <ezzatron@gmail.com>
2020-09-07 13:16:20 +1000

    WIP

--- test/fixture/all-svg/flatten-svg.js
+++ test/fixture/all-svg/flatten-svg.js
@@ -6,7 +6,11 @@ ready(() => {
 
 function createDefs (parent, svgDocuments) {
   const defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs')
-  for (const {documentElement} of svgDocuments) defs.appendChild(documentElement)
+
+  for (const {documentElement} of svgDocuments) {
+    defs.appendChild(documentElement)
+  }
+
   parent.insertBefore(defs, parent.firstChild)
 }
 
@@ -23,43 +27,44 @@ function findReplaceableUses (svgDocument) {
   return uses
 }
 
+async function fetchChild (documents, fetched, mapping, url) {
+  const documentUrl = new URL(url)
+  documentUrl.hash = ''
+  const documentUrlString = documentUrl.toString()
+
+  if (fetched[documentUrlString]) return
+
+  const response = await fetch(url)
+  const content = await response.text()
+  const child = parseSvg(content)
+
+  if (child) {
+    fetched[documentUrlString] = child
+    rewriteIds(mapping, url, child)
+    documents.push([url, child])
+  }
+}
+
 async function flatten (baseHref, svgDocument) {
   const documents = [[baseHref, svgDocument]]
   const replacements = []
-  const fetchedDocuments = {}
+  const fetched = {}
   const mapping = {}
 
   while (documents.length > 0) {
     const [baseHref, svgDocument] = documents.pop()
+    const uses = findReplaceableUses(svgDocument)
+    const fetches = []
 
-    for (const use of findReplaceableUses(svgDocument)) {
+    for (const use of uses) {
       const href = use.getAttribute('href')
       const hrefUrl = new URL(href, baseHref)
 
-      const documentUrl = new URL(hrefUrl)
-      documentUrl.hash = ''
-      const documentUrlString = documentUrl.toString()
-
-      if (!fetchedDocuments[documentUrlString]) {
-        const response = await fetch(href)
-        const content = await response.text()
-        let child
-
-        try {
-          child = parseSvg(content)
-        } catch (error) {
-          console.error(error)
-
-          continue
-        }
-
-        fetchedDocuments[documentUrlString] = child
-        rewriteIds(mapping, hrefUrl, child)
-        documents.push([hrefUrl, child])
-      }
-
       replacements.push([hrefUrl, use])
+      fetches.push(fetchChild(documents, fetched, mapping, hrefUrl))
     }
+
+    await Promise.all(fetches)
   }
 
   if (replacements.length < 1) return
@@ -70,7 +75,7 @@ async function flatten (baseHref, svgDocument) {
     if (mappedId) use.setAttribute('href', `#${mappedId}`)
   }
 
-  createDefs(svgDocument.documentElement, Object.values(fetchedDocuments))
+  createDefs(svgDocument.documentElement, Object.values(fetched))
 }
 
 async function main () {
@@ -83,7 +88,7 @@ function parseSvg (string) {
   const document = domParser.parseFromString(string, 'text/xml')
   const errors = document.getElementsByTagName('parsererror')
 
-  if (errors.length) throw new Error('Unable to parse SVG')
+  if (errors.length) return undefined
 
   return document
 }
