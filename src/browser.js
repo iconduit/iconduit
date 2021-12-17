31058ff8e0b668f1cd7430fa60075252f7fec9b0
Erin Millard <ezzatron@gmail.com>
2021-12-17 11:29:19 +1000

    Replace puppeteer-cluster with a manual pool

--- src/browser.js
+++ src/browser.js
@@ -1,50 +1,104 @@
-import {Cluster} from 'puppeteer-cluster'
+import puppeteer from 'puppeteer'
 
 import {DEFAULT_BROWSER_CONCURRENCY, DEFAULT_BROWSER_TIMEOUT} from './constant.js'
 
 export function createBrowserManager (env, logger, retryOperation) {
   const {BROWSER_CONCURRENCY: envConcurrency, BROWSER_TIMEOUT: envTimeout} = env
+  const concurrency = envConcurrency ? parseInt(envConcurrency, 10) : DEFAULT_BROWSER_CONCURRENCY
+  const timeout = envTimeout ? parseInt(envTimeout, 10) : DEFAULT_BROWSER_TIMEOUT
+  const subscribers = new Set()
+  let available, browser, pages
 
   const puppeteerOptions = {
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security'],
     pipe: true,
+    timeout,
   }
 
-  const clusterOptions = {
-    concurrency: Cluster.CONCURRENCY_PAGE,
-    maxConcurrency: envConcurrency ? parseInt(envConcurrency, 10) : DEFAULT_BROWSER_CONCURRENCY,
-    puppeteerOptions,
-    timeout: envTimeout ? parseInt(envTimeout, 10) : DEFAULT_BROWSER_TIMEOUT,
+  return {
+    async run (fn) {
+      if (browser) throw new Error('Browser manager already started')
+
+      logger.debug(`Launching browser with ${concurrency} pages`)
+      browser = await puppeteer.launch(puppeteerOptions)
+
+      pages = await Promise.all(Array.from(Array(concurrency)).map(async () => {
+        const page = await browser.newPage()
+        page.setDefaultTimeout(timeout)
+
+        return page
+      }))
+
+      available = new Set(pages)
+      let result
+
+      try {
+        result = await fn()
+      } finally {
+        await Promise.all(pages.map(page => page.close()))
+        await browser.close()
+
+        browser = undefined
+        pages = undefined
+      }
+
+      return result
+    },
+
+    async withPage (fn) {
+      if (!browser) throw new Error('Browser manager not started')
+
+      const page = await acquirePage()
+      let result
+
+      try {
+        result = await retryOperation(() => fn(page))
+      } finally {
+        releasePage(page)
+      }
+
+      return result
+    },
   }
 
-  logger.debug(`Concurrency is currently set to ${clusterOptions.maxConcurrency}`)
+  async function acquirePage () {
+    logger.debug('Attempting to acquire a browser page')
 
-  let cluster = null
-  const manager = {run, withPage}
+    while (true) {
+      // eslint-disable-next-line no-unreachable-loop
+      for (const page of available) {
+        logger.debug(`Browser page ${pageNumber(page)} acquired`)
+        available.delete(page)
 
-  return manager
+        return page
+      }
 
-  async function run (fn) {
-    if (cluster) throw new Error('Browser manager already started')
+      await nextPage()
+    }
+  }
 
-    cluster = await Cluster.launch(clusterOptions)
-    let result
+  function nextPage () {
+    let res
+    const promise = new Promise(resolve => { res = resolve })
 
-    try {
-      result = await fn()
-    } finally {
-      await cluster.idle()
-      await cluster.close()
-
-      cluster = null
+    function subscriber () {
+      subscribers.delete(subscriber)
+      res()
     }
 
-    return result
+    subscribers.add(subscriber)
+
+    return promise
   }
 
-  function withPage (fn) {
-    if (!cluster) throw new Error('Browser manager not started')
+  function pageNumber (page) {
+    return pages.indexOf(page) + 1
+  }
 
-    return retryOperation(async function clusterExecute () { return cluster.execute(({page}) => fn(page)) })
+  function releasePage (page) {
+    logger.debug(`Releasing browser page ${pageNumber(page)}`)
+
+    available.add(page)
+    subscribers.forEach(subscriber => { subscriber() })
   }
 }
