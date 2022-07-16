657c00b6998885b48ad9c45cae4de94cdd31091b
Erin Millard <contact@erin.id.au>
2022-07-16 19:09:01 +1000

    Unwrap external <use> elements

--- fix-svg-use.js
+++ fix-svg-use.js
@@ -1,14 +1,73 @@
+const SVG = "http://www.w3.org/2000/svg";
+let useNumber = 0;
+
 main().catch((error) => {
   console.error(error);
 });
 
 async function main() {
-  await ready();
+  await domReady();
 
-  console.log("ready");
+  for (let i = 0; i < 100; ++i) {
+    const uses = findUses();
+
+    if (uses.length < 1) {
+      break;
+    }
+
+    await flattenUses(uses);
+  }
 }
 
-function ready() {
+function findUses() {
+  const uses = document.getElementsByTagNameNS(SVG, "use");
+
+  return [...uses].filter((use) => !use.getAttribute("href").startsWith("#"));
+}
+
+async function flattenUses(uses) {
+  await Promise.all(uses.map((use) => flattenUse(use)));
+}
+
+async function flattenUse(use) {
+  console.log("Flattening", use);
+
+  const href = new URL(use.getAttribute("href"), window.location.href);
+
+  try {
+    const svg = await loadSvg(href);
+    const symbol = createSymbolForUse(use, svg);
+
+    use.parentNode.insertBefore(symbol, use);
+    use.setAttribute("href", `#${symbol.getAttribute("id")}`);
+  } catch {}
+}
+
+async function loadSvg(href) {
+  const res = await fetch(href);
+
+  if (!res.ok) {
+    throw new Error(
+      `Unexpected HTTP status ${res.status} when fetching ${href}`
+    );
+  }
+
+  const domParser = new DOMParser();
+  const document = domParser.parseFromString(await res.text(), "text/xml");
+
+  return document.documentElement;
+}
+
+function createSymbolForUse(use, svg) {
+  const symbol = document.createElementNS(SVG, "symbol");
+  symbol.setAttribute("id", `__fix_svg_use_${useNumber++}`);
+  symbol.setAttribute("viewBox", svg.getAttribute("viewBox"));
+  symbol.replaceChildren(...svg.children);
+
+  return symbol;
+}
+
+function domReady() {
   return new Promise((resolve) => {
     if (document.readyState !== "loading") {
       resolve();
