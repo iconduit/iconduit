2f54368151c02755640e00d98092f5379a970faa
Erin Millard <contact@erin.id.au>
2022-07-17 15:19:54 +1000

    Identify all references

--- fix-svg-use.ts
+++ fix-svg-use.ts
@@ -1,101 +1,134 @@
 const NS_SVG = "http://www.w3.org/2000/svg";
 const NS_XLINK = "http://www.w3.org/1999/xlink";
+
 const SVG_REFERENCE_HREF_TAG_NAMES = [
-    "image",
-    "linearGradient",
-    "pattern",
-    "radialGradient",
-    "textPath",
-    "use",
+  "image",
+  "linearGradient",
+  "pattern",
+  "radialGradient",
+  "textPath",
+  "use",
 ];
+
 const SVG_REFERENCE_ATTRIBUTES = [
-    "clip-path",
-    "fill",
-    "filter",
-    "marker-end",
-    "marker-mid",
-    "marker-start",
-    "mask",
-    "stroke",
+  "clip-path",
+  "fill",
+  "filter",
+  "marker-end",
+  "marker-mid",
+  "marker-start",
+  "mask",
+  "stroke",
 ];
+
 main().catch((error) => {
-    console.error(error);
+  console.error(error);
 });
+
 async function main() {
-    await domReady();
-    const svgLoader = createSvgLoader();
-    await svgLoader.loadSvgs();
+  await domReady();
+
+  const svgLoader = createSvgLoader();
+  await svgLoader.loadSvgs();
 }
+
 function createSvgLoader() {
-    return {
-        async loadSvgs() {
-            const documentHref = new URL(window.location.href);
-            for (const svg of document.getElementsByTagName("svg")) {
-                console.log(findReferences(documentHref, svg));
-            }
-        },
-    };
-    function findReferences(documentHref, svg) {
-        const references = new Set();
-        for (const tagName of SVG_REFERENCE_HREF_TAG_NAMES) {
-            const elements = svg.getElementsByTagName(tagName);
-            for (const element of elements) {
-                const href = element.getAttribute("href") ?? "";
-                const xlinkHref = element.getAttributeNS(NS_XLINK, "href") ?? "";
-                if (href) {
-                    references.add(new URL(href, documentHref).toString());
-                }
-                if (xlinkHref) {
-                    references.add(new URL(xlinkHref, documentHref).toString());
-                }
-            }
+  return {
+    async loadSvgs() {
+      const documentHref = new URL(window.location.href);
+
+      for (const svg of document.getElementsByTagName("svg")) {
+        console.log(findReferences(documentHref, svg));
+      }
+    },
+  };
+
+  function findReferences(documentHref: URL, svg: SVGSVGElement) {
+    const references = new Set<string>();
+
+    for (const tagName of SVG_REFERENCE_HREF_TAG_NAMES) {
+      const elements = svg.getElementsByTagName(tagName);
+
+      for (const element of elements) {
+        const href = element.getAttribute("href") ?? "";
+        const xlinkHref = element.getAttributeNS(NS_XLINK, "href") ?? "";
+
+        if (href) {
+          references.add(new URL(href, documentHref).toString());
         }
-        for (const attribute of SVG_REFERENCE_ATTRIBUTES) {
-            const elements = svg.querySelectorAll(`[${attribute}]:not([${attribute}=""]`);
-            for (const element of elements) {
-                const reference = element.getAttribute(attribute) ?? "";
-                if (reference) {
-                    references.add(new URL(reference, documentHref).toString());
-                }
-            }
+        if (xlinkHref) {
+          references.add(new URL(xlinkHref, documentHref).toString());
         }
-        return references;
+      }
     }
+
+    for (const attribute of SVG_REFERENCE_ATTRIBUTES) {
+      const elements = svg.querySelectorAll(
+        `[${attribute}]:not([${attribute}=""]`
+      );
+
+      for (const element of elements) {
+        const reference = element.getAttribute(attribute) ?? "";
+
+        if (reference) {
+          references.add(new URL(reference, documentHref).toString());
+        }
+      }
+    }
+
+    return references;
+  }
 }
+
 // function findExternalUses() {
 //   const uses = document.getElementsByTagNameNS(SVG, "use");
+
 //   return [...uses].filter((use) => {
 //     const hrefAttribute = use.getAttribute("href");
+
 //     if (hrefAttribute.startsWith("#")) return false;
+
 //     const href = new URL(hrefAttribute, window.location.href);
 //     href.hash = "";
+
 //     return seenHrefs[href.toString()] == null;
 //   });
 // }
+
 // async function flattenUses(uses) {
 //   await Promise.all(uses.map((use) => flattenUse(use)));
 // }
+
 // async function flattenUse(use) {
 //   const hrefAttribute = use.getAttribute("href");
 //   const svgHref = new URL(hrefAttribute, window.location.href);
 //   svgHref.hash = "";
 //   const svg = await loadSvg(svgHref);
+
 //   if (svg == null) return;
+
 //   const symbol = createSymbolFromSvg(svg, svgHref);
 //   use.ownerDocument.documentElement.appendChild(symbol);
+
 //   const useHref = new URL(hrefAttribute, window.location.href);
 //   const mappedId = idMap[useHref.toString()];
+
 //   use.setAttribute("href", `#${mappedId}`);
 //   use.dataset.__fixSvgUseHref = shortenUrl(useHref);
 // }
+
 // async function loadSvg(href) {
 //   seenHrefs[href.toString()] = true;
 //   const res = await fetch(href);
+
 //   if (!res.ok) return undefined;
+
 //   const domParser = new DOMParser();
 //   const document = domParser.parseFromString(await res.text(), "text/xml");
+
 //   return document.documentElement;
 // }
+
 // function createSymbolFromSvg(svg, href) {
 //   const symbol = document.createElementNS(SVG, "symbol");
 //   symbol.setAttribute("id", svg.getAttribute("id") ?? "");
@@ -103,15 +136,21 @@ function createSvgLoader() {
 //   symbol.replaceChildren(...svg.children);
 //   symbol.dataset.__fixSvgUseHref = shortenUrl(href);
 //   remapId(symbol, href);
+
 //   for (const element of symbol.querySelectorAll('*[id]:not([id=""])')) {
 //     remapId(element, href);
 //   }
+
 //   remapAllUrlReferences(symbol, "fill", href);
+
 //   return symbol;
 // }
+
 // function remapId(element, documentHref) {
 //   const elementId = element.getAttribute("id");
+
 //   if (!elementId) return;
+
 //   const anchor = `#${encodeURIComponent(element.getAttribute("id"))}`;
 //   const elementHref = new URL(anchor, documentHref);
 //   const mappedId = nextId();
@@ -119,21 +158,25 @@ function createSvgLoader() {
 //   element.setAttribute("id", mappedId);
 //   element.dataset.__fixSvgUseId = elementId;
 // }
+
 // function remapAllUrlReferences(node, type, documentHref) {
 //   for (const element of node.querySelectorAll(`*[${type}]:not([${type}=""])`)) {
 //     const remappedAttribute = remapUrlReference(
 //       element.getAttribute(type) ?? "",
 //       documentHref
 //     );
+
 //     if (remappedAttribute) {
 //       element.setAttribute(type, remappedAttribute);
 //     } else {
 //       element.removeAttribute(type);
 //     }
+
 //     const remappedStyle = remapUrlReference(
 //       element.style[type] ?? "",
 //       documentHref
 //     );
+
 //     if (remappedStyle) {
 //       element.style[type] = remappedStyle;
 //     } else {
@@ -141,31 +184,39 @@ function createSvgLoader() {
 //     }
 //   }
 // }
+
 // function remapUrlReference(reference, documentHref) {
 //   const match = reference.match(/^url\(([^)]+)\)$/);
+
 //   if (!match) return reference;
+
 //   const [, content] = match;
 //   const href = content.startsWith('"') ? JSON.parse(content) : content;
 //   const fullHref = new URL(href, documentHref);
+
 //   const mappedId = idMap[fullHref.toString()];
+
 //   return mappedId ? `url(${JSON.stringify(`#${mappedId}`)})` : undefined;
 // }
+
 // function shortenUrl(url) {
 //   if (url.origin !== window.location.origin) return url.toString();
+
 //   return url.toString().substring(url.origin.length);
 // }
+
 // function nextId() {
 //   return `__fix_svg_use_${idSequence++}`;
 // }
+
 function domReady() {
-    return new Promise((resolve) => {
-        if (document.readyState !== "loading") {
-            resolve();
-        }
-        else {
-            document.addEventListener("DOMContentLoaded", () => {
-                resolve();
-            });
-        }
-    });
+  return new Promise<void>((resolve) => {
+    if (document.readyState !== "loading") {
+      resolve();
+    } else {
+      document.addEventListener("DOMContentLoaded", () => {
+        resolve();
+      });
+    }
+  });
 }
